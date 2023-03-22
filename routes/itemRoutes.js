c6c6bd74ac1769e19013cf8c89ba3edaab7cc081
Pacharaphon Plathong <pacharaphon2014@gmail.com>
2023-03-22 12:35:49 +0700

    second

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -7,5 +7,5 @@ router.get("/", itemsController.getItems);
 router.get("/members", itemsController.getGroupMembers);
 router.post("/", itemsController.addItem);
 router.delete("/:item_id", itemsController.deleteItem);
-
+//router.put("/:item_id", itemsController.updateItem)
 module.exports = router;
